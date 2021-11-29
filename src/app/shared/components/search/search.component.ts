11af3d382ef4f328dac9d4299d7e5a390cbb27ef
LucianaMonardez <lucianapiomonardez@gmail.com>
2021-11-29 03:19:58 -0300

    Added search ui

---
+++ src/app/shared/components/search/search.component.ts
@@ -0,0 +1,17 @@
+import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  search = new FormControl('')
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
